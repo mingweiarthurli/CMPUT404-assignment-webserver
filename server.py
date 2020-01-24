f18d026220124fc8eab89d5a80c3a2e117e8b11e
Mingwei Li <mingweiarthurli@gmail.com>
2020-01-24 16:07:55 -0700

    initial commit

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import re
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +30,100 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    def detect_method(self):
+        methods = re.findall(r'^(.+?) ', self.data.decode('utf-8'))
+        method = methods[0]
+
+        # only accept GET
+        if method == 'GET':
+            return method
+        else:
+            return 405
+
+    def get_path(self):
+        # find all matched file names by regular expression
+        paths = re.findall(r'GET (.+?) HTTP', self.data.decode('utf-8'))
+        path = paths[0]       # get the first match
+
+        # add relative directory of ./www folder
+        path = 'www' + path
+        
+        # if the file name end with '/' then return index.html 
+        if path[-1] == '/':
+            path += 'index.html'
+
+        return path
+    
+    def check_path(self, path):
+        dir_status = os.path.isdir(path)
+        file_status = os.path.isfile(path)
+
+        # check if the given path is a sub directory of 'www' foler for security reason
+        input_path = os.path.realpath(path)
+        web_path = os.path.realpath('www')
+        sub_path = input_path.startswith(web_path)
+
+        if dir_status and not file_status and sub_path:
+            return 301
+        elif not dir_status and file_status and sub_path:
+            return 200
+        else:
+            return 404
+
+    def get_mime_type(self, path):
+        # path = 'www/test.org/index.html'
+        # find all matched file type by regular expression
+
+        if path.endswith('.html'):
+            mime_type = 'text/html'
+        elif path.endswith('.css'):
+            mime_type = 'text/css'
+        else:
+            mime_type = ''
+
+        return mime_type
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # self.request.sendall(bytearray("OK",'utf-8'))
+
+        # parse request
+        try:
+            method = self.detect_method()
+            if method == 405:
+                raise Exception(405, '405 Method Not Allowed')
+
+            path = self.get_path()
+            # check if the file exists
+            path_status = self.check_path(path)
+            if path_status == 301:
+                raise Exception(301, path[4:] + '/')       # return a relative URL
+            elif path_status == 404:
+                raise Exception(404, '404 Not Found')
+
+            mime_type = self.get_mime_type(path)
+
+            content_length = os.path.getsize(path)
+
+            with open(path, 'r') as rfile:
+                rfile_data = rfile.read()
+
+            reply = 'HTTP/1.1 200 OK\r\nContent-Length: ' + str(content_length) + '\r\nContent-Type: ' + mime_type + '\r\n\r\n' + rfile_data
+        except Exception as e:
+            code, info = e.args
+            
+            if code == 405:
+                reply = 'HTTP/1.1 405 Method Not Allowed'
+            elif code == 301:
+                body = '<!doctype html><title>301 Moved Permanently</title><h1>301 Moved Permanently</h1>The document has moved<A HREF=\"' + info + '\">here</A>'
+                reply = 'HTTP/1.1 301 Moved Permanently\r\nLocation: ' + info + '\r\nContent-Length: ' + str(len(body.encode('utf-8'))) + '\r\nContent-Type: text/html\r\n\r\n' + body
+            elif code == 404:
+                body = '<!doctype html><title>404 Not Found</title><h1>404 Not Found</h1>The document has moved'
+                reply = 'HTTP/1.1 404 Not Found\r\nContent-Length: ' + str(len(body.encode('utf-8'))) + '\r\nContent-Type: text/html\r\n\r\n' + body
+        finally:
+            self.request.sendall(bytearray(reply,'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
